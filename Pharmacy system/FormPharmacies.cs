2d3c4c245bcbbe690d24fb28e7a0ae9f8c2d895f
artis.butorin@yandex.ru <artis.butorin@yandex.ru>
2020-07-03 13:24:59 +0300

    UpdateSystematick

--- Pharmacy system/FormPharmacies.cs
+++ Pharmacy system/FormPharmacies.cs
@@ -33,7 +33,7 @@ namespace Pharmacy_system
                 EmptyField();
                 ShowPharmacies();
             }
-            catch { }
+            catch { MessageBox.Show("Невозможно добавить информацию об аптеке", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
         }
 
         private void listViewShowPharmacies_SelectedIndexChanged(object sender, EventArgs e)
